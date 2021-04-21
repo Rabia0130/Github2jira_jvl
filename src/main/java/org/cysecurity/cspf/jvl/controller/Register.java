555b0537df98424bc9c2d04078c0531276af2777
Rabia0130 <rabia.khanfir@checkmarx.com>
2021-04-21 09:51:58 +0200

    test again, should work. second scan before fix

--- src/main/java/org/cysecurity/cspf/jvl/controller/Register.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/Register.java
@@ -5,7 +5,7 @@
  */
 
 package org.cysecurity.cspf.jvl.controller;
-import java.sql.PreparedStatement;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -54,26 +54,9 @@ public class Register extends HttpServlet {
                 if(con!=null && !con.isClosed())
                 {
 
-                    //Statement stmt = con.createStatement();
-                    //stmt.executeUpdate("INSERT into users(username, password, email, About,avatar,privilege,secretquestion,secret) values ('"+user+"','"+pass+"','"+email+"','"+about+"','default.jpg','user',1,'"+secret+"')");
-                    String query = "INSERT into users(" +" username," + " password," + "email," + "About," + "avatar," + "privilege," + "secretquestion," + "secret) values (" + "?,?,?,?,?,?,?,?)";
-                    PreparedStatement stmt = con.prepareStatement(query);
-                    stmt.setString(1, user);
-                    stmt.setString(2, pass);
-                    stmt.setString(3, email);
-                    stmt.setString(4, about);
-                    stmt.setString(8, secret);
-
-// execute the preparedstatement insert
-
-                    stmt.executeUpdate();
-
-                    String sql = "INSERT into UserMessages(recipient, sender, subject, msg) values ("+ "?,'admin','Hi','Hi<br/> This is admin of this page. <br/> Welcome to Our Forum')";
-                    PreparedStatement st = con.prepareStatement(sql);
-                    //Statement st = con.prepareStatement();
-                    st.setString(1, user);
-                    st.executeUpdate();
-                    //st.executeUpdate("INSERT into UserMessages(recipient, sender, subject, msg) values ('"+user+"','admin','Hi','Hi<br/> This is admin of this page. <br/> Welcome to Our Forum')");
+                    Statement stmt = con.createStatement();
+                    stmt.executeUpdate("INSERT into users(username, password, email, About,avatar,privilege,secretquestion,secret) values ('"+user+"','"+pass+"','"+email+"','"+about+"','default.jpg','user',1,'"+secret+"')");
+                    stmt.executeUpdate("INSERT into UserMessages(recipient, sender, subject, msg) values ('"+user+"','admin','Hi','Hi<br/> This is admin of this page. <br/> Welcome to Our Forum')");
 
                     response.sendRedirect("index.jsp");
 
@@ -137,3 +120,4 @@ public class Register extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
+}
